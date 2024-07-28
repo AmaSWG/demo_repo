37444eeefe102acdc0c57a3bd508ac7efb7af1a1
AmaSWG <amayagunasekara4@gmail.com>
2024-07-28 18:16:41 +0530

    Added some comments

--- andoperator.c
+++ andoperator.c
@@ -9,9 +9,13 @@ int main(){
     bool sunny = true;
 
     if(temp >= 0 && temp <= 30 && sunny){
+
+	//this block gets executed only if both conditions are true
         printf("The weather is good!");
     }
     else{
+	
+	//this block gets executed if one or both conditions are false
         printf("The weather is bad!");
     }
 
